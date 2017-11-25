1d39864964ba8ed447892356e1ae8ba688068cc4
tonwell <wellington.pbi10@gmail.com>
2017-11-25 17:22:05 -0200

    projeto reconfigurado, sem necessidade de Android Studio mais. Ainda com uns bugs pra resolver

--- CrowdMap/app/build/generated/source/buildConfig/debug/droid/crowdmap/BuildConfig.java
+++ CrowdMap/app/build/generated/source/buildConfig/debug/droid/crowdmap/BuildConfig.java
@@ -5,9 +5,9 @@ package droid.crowdmap;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "droid.droid.crowdmap";
+  public static final String APPLICATION_ID = "droid.crowdmap";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
-  public static final String VERSION_NAME = "";
+  public static final String VERSION_NAME = "1.0";
 }
